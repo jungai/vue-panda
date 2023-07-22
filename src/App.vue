94e6407b0872d9d3a4c12947cb49a08eef692959
jungai <juworaphol125@gmail.com>
2023-07-22 15:00:01 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { css } from "../styled-system/css";
+</script>
+
+<template>
+  <div>
+    <h1
+      :class="
+        css({ fontSize: 12, textDecoration: 'underline', color: 'tomato' })
+      "
+    >
+      Hello
+    </h1>
+  </div>
+</template>
+
+<style scoped></style>
